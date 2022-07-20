b3e20931606069b17fce2be98a4be67af7a59c77
bhoyle48 <65142567+bhoyle48@users.noreply.github.com>
2022-07-20 18:04:26 -0500

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn import datasets
 import time
 
 
@@ -9,20 +10,10 @@ st.title('This is my first application')
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 
-d = []
-for p in range(0,51):
-    d.append(
-        {
-            'N': p,
-            'N^2': p**2,
-            'N^3':  p**3
-        }
-    )
-       
-    time.sleep(.1)
+iris = datasets.load_iris()
 
 
-data = pd.DataFrame(d)
+data = iris
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
