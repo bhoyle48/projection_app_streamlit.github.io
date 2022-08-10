f7203dc3234650978270a9dafb41b4627c3f7c99
bhoyle48 <65142567+bhoyle48@users.noreply.github.com>
2022-08-09 19:59:09 -0500

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.set_page_config(layout="wide")
-st.set_page_config(page_title="Forecasting Time Series Data")
 
 with open ("/style.css" ) as css:
         st.marktdown(f'<style>{css.read()}</style>', unsafe_allow_html=True
