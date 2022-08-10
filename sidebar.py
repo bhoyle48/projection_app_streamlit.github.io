8a7472e426eae6375bce291096ffcf6b20eef674
bhoyle48 <65142567+bhoyle48@users.noreply.github.com>
2022-08-09 20:46:40 -0500

    Create sidebar.py
    Add file for sidebar design and content

---
+++ sidebar.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# Set sidebar contact info and about information
+st.sidebar.title("About")
+st.sidebar.info(
+    """
+    Web App URL: <https://bhoyle48.github.io/streamlit-projection-app/> \n
+    GitHub repository: <https://github.com/bhoyle48/streamlit-projection-app>
+    """
+)
+
+st.sidebar.title("Contact")
+st.sidebar.info(
+    """
+    Benjamin Hoyle          
+    [GitHub](https://github.com/bhoyle48) | [LinkedIn](https://www.linkedin.com/in/hoyle-benjamin/) | [Email](Mailto:benjamin.hoyle1598@gmail.com?)
+    """
+)
