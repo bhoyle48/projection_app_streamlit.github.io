726ddf9d4e01e68e7b945cb338557ce649974a6d
bhoyle48 <65142567+bhoyle48@users.noreply.github.com>
2022-08-09 21:11:29 -0500

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -1,18 +1,28 @@
 import streamlit as st
 
-# Set sidebar contact info and about information
-st.sidebar.title("About")
-st.sidebar.info(
-    """
-    Web App URL: <https://bhoyle48.github.io/streamlit-projection-app/> \n
-    GitHub repository: <https://github.com/bhoyle48/streamlit-projection-app>
-    """
-)
-
-st.sidebar.title("Contact")
-st.sidebar.info(
-    """
-    Benjamin Hoyle          
-    [GitHub](https://github.com/bhoyle48) | [LinkedIn](https://www.linkedin.com/in/hoyle-benjamin/) | [Email](Mailto:benjamin.hoyle1598@gmail.com?)
-    """
-)
+def mysidebar():
+    
+    ## GET STYLING from Style.css
+    with open ("style.css" ) as css:
+        st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+        st.write(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+    
+    ## SET SIDEBAR DEFINITION
+    
+    # App About Information
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        Web App URL: <https://bhoyle48.github.io/streamlit-projection-app/> \n
+        GitHub repository: <https://github.com/bhoyle48/streamlit-projection-app>
+        """
+    )
+    
+    # Contact Information
+    st.sidebar.title("Contact")
+    st.sidebar.info(
+        """
+        Benjamin Hoyle          
+        [GitHub](https://github.com/bhoyle48) | [LinkedIn](https://www.linkedin.com/in/hoyle-benjamin/) | [Email](Mailto:benjamin.hoyle1598@gmail.com?)
+        """
+    )
