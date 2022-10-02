10c4f91a1ca9e39e161db708eae8e9f3883be185
bhoyle48 <benjamin.hoyle1598@gmail.com>
2022-10-01 20:42:08 -0500

    ARIMA

--- sidebar.py
+++ sidebar.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-# def mysidebar():
+def mysidebar():
     
     ## GET STYLING from Style.css
     with open ("style.css" ) as css:
         st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
         st.write(f'<style>{css.read()}</style>', unsafe_allow_html=True)
     
-## SET SIDEBAR DEFINITION
+    ## SET SIDEBAR DEFINITION
     
-# App About Information
-st.sidebar.title("About")
-st.sidebar.info(
+#   # App About Information
+    st.sidebar.title("About")
+    st.sidebar.info(
         """
         Web App URL: [bhoyle48.github.io/forecasting](https://bhoyle48.github.io/forecasting) \n
         GitHub repository: [github.com/bhoyle48/forecasting](https://github.com/bhoyle48/forecasting)
